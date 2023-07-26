6fe1a5303fd5f3373709d810f75cd34a26c93e51
Trungdev1611 <103551327+Trungdev1611@users.noreply.github.com>
2023-07-26 11:47:11 +0700

    update status code and message in response

--- src/main/java/api/restful/first_app/students/StudentController.java
+++ src/main/java/api/restful/first_app/students/StudentController.java
@@ -3,6 +3,8 @@ package api.restful.first_app.students;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import api.restful.first_app.common.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,33 +18,37 @@ public class StudentController {
     // Controller - chịu trách nhiệm nhận request và trả về reponse cho client. Việc
     // xử lý dữ liệu để cho bước này được xử lý ở service
     // nó là trung gian của client và Service
-    private StudentSerrvice studentSerrvice;
+    private final StudentSerrvice studentSerrvice;
 
     public StudentController(StudentSerrvice studentSerrvice) {
         this.studentSerrvice = studentSerrvice;
     }
 
     @GetMapping("/students")
-    public ArrayList<Student> getStudent() {
+    // ResponseEntity<?> sử dụng dấu ? để cho kiểu dữ liệu tùy ý - giống any bên
+    // typescript
+    public ResponseEntity<ApiResponse> getStudent() { // sử dụng ResponseEntity để custom request với status
         return studentSerrvice.getAll();
 
     }
 
     @PostMapping("/students")
-    public Student AddStudent(@RequestBody Student student) { // lấy từ body ra student và trả về đối tượng đó
+    public ResponseEntity<ApiResponse> AddStudent(@RequestBody Student student) { // lấy từ body ra student và trả về
+                                                                                  // đối tượng đó
         System.out.println("Student:::" + student.toString());
         return studentSerrvice.addStudent(student);
     }
 
     @PutMapping("/students/{id}")
-    public Optional<Student> UpdateStudent(@PathVariable int id, @RequestBody Student student) { // Optional cho phép
-                                                                                                 // trả về giá trị null
+    public ResponseEntity<ApiResponse> UpdateStudent(@PathVariable int id, @RequestBody Student student) { // Optional
+                                                                                                           // cho phép
+        // trả về giá trị null
         System.out.println("Student:::" + student.toString());
         return studentSerrvice.updateStudent(id, student);
     }
 
     @DeleteMapping("/students/{id}")
-    public Optional<Student> DeleteStudent(@PathVariable int id) {
+    public ResponseEntity<ApiResponse> DeleteStudent(@PathVariable int id) {
         return studentSerrvice.deleteStudent(id);
 
     }
